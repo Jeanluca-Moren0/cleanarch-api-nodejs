8d1934528b96070308efaf4386e14ab9498d7692
Jeanluca-Moren0 <jeanlucam12@gmail.com>
2022-04-30 18:10:23 -0300

    ensure LoadUserByEmailRepository returns null if no user is found

---
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -0,0 +1,12 @@
+class LoadUserByEmailRepository {
+  async load (email) {
+    return null
+  }
+}
+describe('LoadUserByEmailRepository', () => {
+  test('Should return null if no user is found', async () => {
+    const sut = new LoadUserByEmailRepository()
+    const user = await sut.load('invalid@email.com')
+    expect(user).toBeNull()
+  })
+})
