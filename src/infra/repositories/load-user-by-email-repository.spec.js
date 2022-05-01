27d4938044460e7543c10f875afb3ac47370729b
Jeanluca-Moren0 <jeanlucam12@gmail.com>
2022-05-01 13:24:00 -0300

    ensure LoadUserByEmailRepository throws if no userModel is provided

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -45,4 +45,10 @@ describe('LoadUserByEmailRepository', () => {
     const user = await sut.load('valid@email.com')
     expect(user._id).toEqual(fakeUser.insertedId)
   })
+
+  test('Should throw if no userModel is provided', async () => {
+    const sut = new LoadUserByEmailRepository()
+    const promise = sut.load('any@email.com')
+    expect(promise).rejects.toThrow()
+  })
 })
