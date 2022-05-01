e6b7d10073c9811442f16c2aff9735ad38e07d49
Jeanluca-Moren0 <jeanlucam12@gmail.com>
2022-05-01 16:06:26 -0300

    changing jest config to delete test:integration bugs

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
-
 module.exports = {
-
-  collectCoverage: true,
   coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   coverageProvider: 'v8',
   collectCoverageFrom: ['**/src/**/*.js'],
   preset: '@shelf/jest-mongodb'
