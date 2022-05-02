172cc86f66fb1beec592ec8d3cefbaf722780011
Jeanluca-Moren0 <jeanlucam12@gmail.com>
2022-05-02 12:28:13 -0300

    move variable to global scope

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -1,8 +1,7 @@
 const request = require('supertest')
-
+let app
 
 describe('Content-type Middleware', () => {
-  let app
   beforeEach(() => {
     jest.resetModules()
     app = require('../config/app')
@@ -11,25 +10,21 @@ describe('Content-type Middleware', () => {
   test('should return json content type as default', async () => {
     app.get('/test_content_type', (req, res) => {
       res.send('')
-      
     })
 
     await request(app)
       .get('/test_content_type')
       .expect('content-type', /json/)
-    
   })
 
   test('should return xml content type if requested', async () => {
     app.get('/test_content_type', (req, res) => {
       res.type('text/html')
       res.send('')
-      
     })
-    
+
     await request(app)
       .get('/test_content_type')
       .expect('content-type', /html/)
-    
   })
 })
