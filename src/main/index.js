7c806a2e30fa92de2ac211b8d74e725776f73d97
Jeanluca-Moren0 <jeanlucam12@gmail.com>
2022-05-01 23:45:15 -0300

    add routes file

--- src/main/index.js
+++ src/main/index.js
@@ -1,11 +1,3 @@
+const app = require('./config/app')
 
-const MongoHelper = require('../infra/helpers/mongo-helper')
-const env = require('./config/env')
-
-
-MongoHelper.connect(env.mongoUrl).then(() => {
-  const app = require('./config/app')
-  app.listen(3333, () => console.log('Server Running ⚡'))
-})
-  .catch(console.error)
-
+app.listen(3333, () => console.log('Server Running ⚡'))
